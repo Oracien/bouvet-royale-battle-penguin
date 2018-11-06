50af9a75563dd3c0522260d875026167c7693834
sm0xe <sm0xe@protonmail.com>
2018-11-06 20:23:00 +0100

    Oopsie-doopsie

--- src/main/java/com/function/Minimax.java
+++ src/main/java/com/function/Minimax.java
@@ -3,49 +3,49 @@ package com.function;
 public class Minimax {
 
     private int evaluateState(Match gameState) {
-        int negativeValue = getNegativeValue(Match gameState);
-        return value;
+	int negativeValue = getNegativeValue(Match gameState);
+	return value;
     }
 
     private int getNegativeValue(Match gameState) {
-        int value = 0;
-        if(gameState.you.strength == 0) {
-            value -= 10000;
-        }
-        value -= (500 - gameState.you.strength) * 20;
-        if(isInFire(gameState)) {
-            value -= 500;
-        }
-        if(closeToFire(gameState)) {
-            value -= 50;
-        }
-        if(awayFromEnemy(gameState)) {
-            value -= 50;
-        }
-        return value;
+	int value = 0;
+	if(gameState.you.strength == 0) {
+	    value -= 10000;
+	}
+	value -= (500 - gameState.you.strength) * 20;
+	if(isInFire(gameState)) {
+	    value -= 500;
+	}
+	if(closeToFire(gameState)) {
+	    value -= 50;
+	}
+	if(awayFromEnemy(gameState)) {
+	    value -= 50;
+	}
+	return value;
     }
 
     private int getPositiveValue(Match gameState) {
-        int value = 0;
-        if(gameState.enemies[0].strength == 0) {
-            value += 10000;
-        }
-        value += (500 - gameState.enemies[0].strength) * 20;
-        switch(gameState.you.bonus) {
-            case "weapon-range":
-                value += 100;
-                break;
-            case "weapon-damage":
-                value += 100;
-                break;
-            case "strength":
-                value += 100;
-                break;
-            default:
-                break;
-        }
-        value += (50 - distanceFromCenter);
-        return value;
+	int value = 0;
+	if(gameState.enemies[0].strength == 0) {
+	    value += 10000;
+	}
+	value += (500 - gameState.enemies[0].strength) * 20;
+	switch(gameState.you.bonus) {
+	    case "weapon-range":
+		value += 100;
+		break;
+	    case "weapon-damage":
+		value += 100;
+		break;
+	    case "strength":
+		value += 100;
+		break;
+	    default:
+		break;
+	}
+	value += (50 - distanceFromCenter);
+	return value;
     }
 
     private Action[] getLegalActions(Match gameState, int agentIndex) {
@@ -60,7 +60,7 @@ public class Minimax {
 		    if(!doesCellContainWall(gameState.you.x, --gameState.you.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(gameState.you.x, ++gameState.you.y)){
+		    if(!doesCellContainWall(gameState.you.x, ++gameState.you.y)){
 			actions.add("retreat");
 		    }
 		    break;
@@ -68,7 +68,7 @@ public class Minimax {
 		    if(!doesCellContainWall(gameState.you.x, ++gameState.you.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(gameState.you.x, --gameState.you.y)){
+		    if(!doesCellContainWall(gameState.you.x, --gameState.you.y)){
 			actions.add("retreat");
 		    }
 		    break;
@@ -76,7 +76,7 @@ public class Minimax {
 		    if(!doesCellContainWall(--gameState.you.x, gameState.you.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(++gameState.you.x, gameState.you.y)){
+		    if(!doesCellContainWall(++gameState.you.x, gameState.you.y)){
 			actions.add("retreat");
 		    }
 		    break;
@@ -84,13 +84,13 @@ public class Minimax {
 		    if(!doesCellContainWall(++gameState.you.x, gameState.you.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(--gameState.you.x, gameState.you.y)){
+		    if(!doesCellContainWall(--gameState.you.x, gameState.you.y)){
 			actions.add("retreat");
 		    }
 		    break;
 	    }
 	}
-	else{
+	{
 	    if(gameState.enemy.ammo>0){
 		actions.add("shoot");
 	    }
@@ -100,7 +100,7 @@ public class Minimax {
 		    if(!doesCellContainWall(gameState.enemy.x, --gameState.enemy.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(gameState.enemy.x, ++gameState.enemy.y)){
+		    if(!doesCellContainWall(gameState.enemy.x, ++gameState.enemy.y)){
 			actions.add("retreat");
 		    }
 		    break;
@@ -108,7 +108,7 @@ public class Minimax {
 		    if(!doesCellContainWall(gameState.enemy.x, ++gameState.enemy.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(gameState.enemy.x, --gameState.enemy.y)){
+		    if(!doesCellContainWall(gameState.enemy.x, --gameState.enemy.y)){
 			actions.add("retreat");
 		    }
 		    break;
@@ -116,7 +116,7 @@ public class Minimax {
 		    if(!doesCellContainWall(--gameState.enemy.x, gameState.enemy.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(++gameState.enemy.x, gameState.enemy.y)){
+		    if(!doesCellContainWall(++gameState.enemy.x, gameState.enemy.y)){
 			actions.add("retreat");
 		    }
 		    break;
@@ -124,7 +124,7 @@ public class Minimax {
 		    if(!doesCellContainWall(++gameState.enemy.x, gameState.enemy.y)){
 			actions.add("advance");
 		    }
-		    else if(!doesCellContainWall(--gameState.enemy.x, gameState.enemy.y)){
+		    if(!doesCellContainWall(--gameState.enemy.x, gameState.enemy.y)){
 			actions.add("retreat");
 		    }
 		    break;
@@ -138,23 +138,23 @@ public class Minimax {
     }
 
     public double getValue(Match gameState, int depth, int agentIndex) {
-        if(depth == 0 || getLegalActions(gameState, agentIndex).length == 0) {
-            return evaluateState(gameState);
-        }
-        if (agentIndex == 0) {
-            double value = -1e100;
-            Action[] possibleActions = getLegalActions(gameState, agentIndex);
-            for (Action action : possibleActions) {
-                value = Math.max(value, getValue(generateSuccessorState(gameState, agentIndex, action), depth-1, agentIndex+1));
-            }
-            return value;
-        }
-        double value = 1e100;
-        Action[] possibleActions = getLegalActions(gameState, agentIndex);
-        for (Action action : possibleActions) {
-            value = Math.min(value, getValue(generateSuccessorState(gameState, agentIndex, action), depth-1, agentIndex+1));
-        }
-        return value;
+	if(depth == 0 || getLegalActions(gameState, agentIndex).length == 0) {
+	    return evaluateState(gameState);
+	}
+	if (agentIndex == 0) {
+	    double value = -1e100;
+	    Action[] possibleActions = getLegalActions(gameState, agentIndex);
+	    for (Action action : possibleActions) {
+		value = Math.max(value, getValue(generateSuccessorState(gameState, agentIndex, action), depth-1, agentIndex+1));
+	    }
+	    return value;
+	}
+	double value = 1e100;
+	Action[] possibleActions = getLegalActions(gameState, agentIndex);
+	for (Action action : possibleActions) {
+	    value = Math.min(value, getValue(generateSuccessorState(gameState, agentIndex, action), depth-1, agentIndex+1));
+	}
+	return value;
     }
 
     public Action chooseAction() {
